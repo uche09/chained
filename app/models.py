30651b2062d9c3655e629d33f0462236e2d37eb9
Uche09 <ucheomesu@gmail.com>
2024-11-17 16:18:03 +0100

    added anonymous user_id (anon_id) to links table in database

--- app/models.py
+++ app/models.py
@@ -19,10 +19,11 @@ class Link(db.Model):
     __tablename__ = "links"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index=True) # Registered users id
+    anon_id = db.Column(db.String(255), index=True) # Anonymous users id
     origin_link = db.Column(db.String(2048), nullable=False)
     short_link = db.Column(db.String(15), nullable=False, index=True, unique=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=func.now())
+    created_at = db.Column(db.DateTime, nullable=True, default=func.now())
     clicks = db.Column(db.Integer, default=0)
     last_clicked = (db.DateTime)
 
